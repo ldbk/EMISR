44b1a5a58113c6edbff2b7bc3e6669482f4ca388
ldbk <laurent.dubroca@gmail.com>
2014-12-04 23:10:13 +0100

    vignette added

--- R/EMISR.R
+++ R/EMISR.R
@@ -5,6 +5,8 @@
 #' (GMIS). EMIS and GMIS are two activities of the European Commission - DG
 #' JRC (Joint Research Centre), developed within the Water Resources Unit of
 #' the Institute for Environment and Sustainability (IES).
+#' The name of this package originates from a wordplay in french: 
+#' EMISR reads in french sounds like "\'emissaire" or emissary.
 #'
 #' @docType package
 #' @name EMISR 
